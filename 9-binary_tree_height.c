27e6a96bf9bab35ca54d98d50f0ddec6edd90380
Fortunatekio <fortunatemugendi8@gmail.com>
2023-10-31 23:32:24 +0300

    height corrections

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,6 +17,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height = binary_tree_height(tree->left);
 	size_t right_height = binary_tree_height(tree->right);
 	{
-		return 1 + MAX(left_height, right_height);
+		return (1 + MAX(left_height, right_height));
 	}
 }
