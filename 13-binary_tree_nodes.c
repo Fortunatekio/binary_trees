5877df595d4194e4cc641eb19b04a6f6f0d92691
Fortunatekio <fortunatemugendi8@gmail.com>
2023-11-01 22:30:46 +0300

    counting nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,4 +8,7 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-
+	if (tree == NULL)
+		return (0);
+	return ((tree->left || tree->right) + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
