687de72948d134fee6e972f5994113a2d1213036
Fortunatekio <fortunatemugendi8@gmail.com>
2023-11-02 01:20:32 +0300

    nodes chsnges

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,5 +10,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	return ((tree->left || tree->right) + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	return ((tree->left || tree->right) + binary_tree_nodes
+			(tree->left) + binary_tree_nodes(tree->right));
 }
