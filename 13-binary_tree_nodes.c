935d0d21bdbfb8c392dc1b683233d6ad92a315a3
Fortunatekio <fortunatemugendi8@gmail.com>
2023-11-01 22:26:21 +0300

    binary

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - a function that counts the number of nodes
+ * in a binary tree
+ * @tree: a pointer to the root node of the binary tree
+ * Return: return 0 if tree is NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+
