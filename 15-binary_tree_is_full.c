4d0b87c7d7055ba3446cd3ea60543f1a60adb92e
Fortunatekio <fortunatemugendi8@gmail.com>
2023-11-02 00:17:14 +0300

    full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,7 +9,9 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree == NULL)
 		return (0);
-	return (!tree->left && !tree->right) && binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right);
+	if (!tree->left && !tree->right)
+		return (1);
+	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 }
