73c393d4218b7e4cb3b9b2060ff633ed95ce2fd3
Fortunatekio <fortunatemugendi8@gmail.com>
2023-11-02 15:35:11 +0300

    corrections

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,7 +7,8 @@
  * @second: the second node
  * Return: pointer to the lowest common ancestor node
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
 	binary_tree_t *a, *b;
 
